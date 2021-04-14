635501455d3879e30830db7b71b24d470e7526fd
H-Cavid <cavid.hasanov2@bk.ru>
2021-04-14 12:25:16 +0400

    --->>>API is added---->>>

--- ecommerce/base/urls.py
+++ ecommerce/base/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('api/product/',ProductViews.as_view()),
+    path('api/product/<id>/',ProductDetailViews.as_view()),
     path('add_to_wishlist/<int:id>/', add_to_wishlist, name='add_to_wishlist'),
     path('remove_wishlist/', remove_wishlist, name='remove_wishlist')
 ]
--- ecommerce/base/views.py
+++ ecommerce/base/views.py
@@ -8,17 +8,40 @@ from utils.mixin import DRF
 from django.views.generic import TemplateView,View
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import ProductSerializers
+from .serializers import *
+from rest_framework import status
 # Create your views here.
 
 class ProductViews(APIView):
     def get(self,request):
-        product = Product.objects.all()
-        serializer = ProductSerializers(product,many=True)
+        product=Product.objects.all()#butun productlarimizi getirirk
+        serializer=ProductSerializers(product,many=True)#serializer-in icine prodcutlarimizi gonderirikki bir bir cevirsin
+        return Response(serializer.data)#response kimide serializerin datasini gonderirik
+
+    def post(self,request):
+        serializer=ProductSerializers(data=request.data)##menim serializerim=di productdserializerden gelen dataya
+        if serializer.is_valid():
+            serializer.save()#eger serializer-da data varsa save elyirik
+            return Response(serializer.data,status=status.HTTP_201_CREATED)#created cavabini gonderirik
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)#yoxdussa 400 error qaytaririq
+
+class ProductDetailViews(APIView):
+    def get(self,request,id):
+        product=Product.objects.get(id=id)
+        serializer = ProductDetailSerializers(product)
         return Response(serializer.data)
 
+    def put(self,request,id):
+        serializer=ProductDetailSerializers(data=request.data)##menim serializerim=di productdserializerden gelen dataya
+        if serializer.is_valid():
+            serializer.save()#eger serializer-da data varsa save elyirik
+            return Response(serializer.data,status=status.HTTP_201_CREATED)#created cavabini gonderirik
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)#yoxdussa 400 error qaytaririq
 
-
+    def delete(self,request,id):
+        product=Product.objects.get(id=id)
+        del product
+        return Response({"success":"Mehsul silindi"})
 
 
 
@@ -42,19 +65,19 @@ class ProductViews(APIView):
 
 
 def index(request):
-    categories = Category.objects.prefetch_related('sub_categories').all()
-    c = Category.objects.get(id=1)
-    a = c.r()
-    print(a)
+    # categories = Category.objects.prefetch_related('sub_categories').all()
+    # c = Category.objects.get(id=1)
+    # a = c.r()
+    # print(a)
     
     sliders = Slider.objects.all()
     #request.session['test'] = 10
     print(request.session.items())
     products = Product.objects.all()[:10]
     context = {
-        'categories':categories,
+        # 'categories':categories,
         'sliders':sliders,
-        'c': c,
+        # 'c': c,
         'products':products, 
     }
     return render(request,'index.html', context)
